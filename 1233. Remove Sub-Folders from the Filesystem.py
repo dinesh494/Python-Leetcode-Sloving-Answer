6a2c740c7967eab540b1621920ff4c06d25e2e76
Dinesh Kumar <72722640+dinesh494@users.noreply.github.com>
2025-07-21 10:04:40 +0530

    1233. Remove Sub-Folders from the Filesystem

---
+++ 1233. Remove Sub-Folders from the Filesystem.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeSubfolders(self, folder):
+        folder.sort()
+        res = []
+        
+        for path in folder:
+            if not res or not path.startswith(res[-1] + "/"):
+                res.append(path)
+        
+        return res
